3904654883e18471cf0b50405b34ead818c817ba
ducpm151061 <duc.pm151061@sis.hust.edu.vn>
2021-07-07 00:24:06 +0700

    aht10

---
+++ aht10/main.py
@@ -0,0 +1,13 @@
+from machine import Pin,I2C
+import time
+from aht10 import AHT10
+i2c = I2C(id=0,scl=Pin(9),sda=Pin(8),freq=400000)
+aht10=AHT10(i2c=i2c)
+led = Pin(25, Pin.OUT)
+
+while True:
+    led.value(1)
+    time.sleep(0.5)
+    aht10.print()
+    led.value(0)
+    time.sleep(0.5)
